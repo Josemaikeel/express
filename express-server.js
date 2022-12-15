fb747c317fecf85a773fc042bccdbe0721516885
Josemaikeel <josemicarrasco4@gmail.com>
2022-12-15 23:39:46 +0100

    added routes

--- express-server.js
+++ express-server.js
@@ -11,11 +11,18 @@ const app = express();
 // sending to the client the read file
 
 app.get('/', (req, res) =>{
-    res.sendFile('./static/index.html', {
+     res.sendFile('./static/index.html', {
         root: __dirname
-    });
+    }); 
 });
 
+app.get('/about', (req, res) => {
+    res.send('Me llamo Josemi, un placer')
+});
+
+app.use((req, res) => {
+    res.status(404).send('Lo siento pero no hemos podido resolver su consulta :(')
+})
 
 app.listen(3000);
 console.log(`server on port ${3000}`);
